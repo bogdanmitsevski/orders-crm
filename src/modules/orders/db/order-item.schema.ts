a43c8811eb553d71f2ace5f61c37587ffd8109e3
Bohdan Mitsevskyi <bogdan@langl.com>
2026-07-14 15:13:10 +0300

    updated dto

--- src/modules/orders/db/order-item.schema.ts
+++ src/modules/orders/db/order-item.schema.ts
@@ -1,18 +1,22 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
+import {IsNotEmpty} from "class-validator";
 
 @Schema({ _id: false })
 export class OrderItem {
     @ApiProperty({ example: 'TSHIRT-RED' })
     @Prop({ required: true })
+    @IsNotEmpty()
     sku: string;
 
     @ApiProperty({ example: 2 })
     @Prop({ required: true, min: 1 })
+    @IsNotEmpty()
     quantity: number;
 
     @ApiProperty({ example: 40 })
     @Prop({ required: true, min: 0 })
+    @IsNotEmpty()
     price: number;
 }
 
